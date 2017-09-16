52e3d3490d34af002204345bbdb0bab3496fa32e
Vladimír Kleštinec <klestinec@gmail.com>
2017-09-16 10:10:59 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hungergames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
